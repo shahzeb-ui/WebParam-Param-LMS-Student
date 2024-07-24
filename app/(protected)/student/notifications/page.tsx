edeea4812d891f2067dc1679b3b804f8a5af2309
Bradley Matjie <77038935+bradleymatjie@users.noreply.github.com>
2024-07-25 00:30:15 +0200

    notification is read

--- app/(protected)/student/notifications/page.tsx
+++ app/(protected)/student/notifications/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useEffect, useState } from "react";
 import { Modal } from "react-bootstrap";
-import getNotifications from "@/app/api/notifications/notification";
+import {getNotifications, markNotificationRead} from "@/app/api/notifications/notification";
 import { notificationType } from "@/app/Utils/notificationInterface";
 import Cookies from "universal-cookie";
 
@@ -9,16 +9,20 @@ export default function Notifications() {
   const [showNotification, setShowNotification] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [notifications, setNotifications] = useState<notificationType[]>([]);
-  const [notification, setNotification] = useState<notificationType|null>(null);
+  const [notification, setNotification] = useState<notificationType | null>(null);
+  const [isReadLoader, setIsReadLoader] = useState(false);
   const cookies = new Cookies();
 
   const user = cookies.get("loggedInUser");
 
-
-  async function fetchNotifications(userId:string) {
+  async function fetchNotifications(userId: string) {
+    try {
       const res = await getNotifications(userId);
       setNotifications(res.data.data);
-      console.log('response: ',res.data.data);  
+      console.log('response: ', res.data.data);
+    } catch (error) {
+      console.error("Error fetching notifications:", error);
+    }
   }
 
   useEffect(() => {
@@ -28,13 +32,24 @@ export default function Notifications() {
     }
 
     console.log('response: ', notifications);
-  }, [])
+  }, [isReadLoader])
 
-  function handleShowNotification(notif: notificationType) {
+  async function handleShowNotification(notif: notificationType) {
     const index = notifications.findIndex(alert => alert.id === notif.id);
+    const noti = notifications.find(alert => alert.id === notif.id);
     setCurrentIndex(index);
     setNotification(notif);
+    setIsReadLoader(true);
+    debugger;
     setShowNotification(true);
+
+    if (noti?.isRead == false) {
+      const markAsRead = await markNotificationRead(notif.id);
+      if (markAsRead.status == 200) {
+        setIsReadLoader(false);
+      }
+    }
+
   }
 
   function handleNotificationNext() {
@@ -60,29 +75,37 @@ export default function Notifications() {
         centered>
         <Modal.Header className="modal-header">
           <h5 className="modal-title" id="staticBackdropLabel">
-            Received: <span style={{fontWeight:'400'}}>{notification?.createdAt.split("T")[0]}</span>
+            Received: <span style={{ fontWeight: '400', fontSize:'15px' }}>{notification?.createdAt.split("T")[0]}</span>
           </h5>
           <button type="button" onClick={() => setShowNotification(false)} className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </Modal.Header>
-        <Modal.Body className="modal-body">
-          {notification?.message}
+        {
+            isReadLoader && notification?.isRead == false ? 
+            <div style={{height: '70px', width:'100%',}} className="d-flex justify-content-center align-items-center flex-column gap-2 ">
+              <div className="spinner-border text-dark" role="status"/>
+              <p>opening message...</p>
+            </div>
+              :
+          <>
+          <Modal.Body className="modal-body">
+          <p>{notification?.message}</p>
         </Modal.Body>
         <Modal.Footer>
-          
-          <div className="d-flex justify-content-between align-center w-100" style={{ width:'100%', alignItems:'center'}}>
-              <p className="p-0 m-0">{currentIndex + 1}/{notifications?.length}</p> 
-              <div className="d-flex justify-content-between align-center gap-2">
+          <div className="d-flex justify-content-between align-center w-100" style={{ width: '100%', alignItems: 'center' }}>
+            <p className="p-0 m-0">{currentIndex + 1}/{notifications?.length}</p>
+            <div className="d-flex justify-content-between align-center gap-2">
               <button type="button" onClick={handleNotificationPrev} className="btn btn-lg btn-dark text-light btn-outline-dark">
-            Prev
-          </button>
-          <button type="button" onClick={handleNotificationNext} className="btn btn-lg btn-dark text-light btn-outline-dark">
-            Next
-          </button>    
+                Prev
+              </button>
+              <button type="button" onClick={handleNotificationNext} className="btn btn-lg btn-dark text-light btn-outline-dark">
+                Next
+              </button>
             </div>
           </div>
         </Modal.Footer>
+        </>
+        }
       </Modal>
-
 
       <div className="rbt-dashboard-content bg-color-white rbt-shadow-box">
         <div className="content">
@@ -90,32 +113,47 @@ export default function Notifications() {
             <h4 className="rbt-title-style-3">Notifications</h4>
           </div>
           <div className="advance-tab-button mb--30">
-            
           </div>
           <div className="tab-content">
             <div className="tab-pane fade active show" id="received" role="tabpanel" aria-labelledby="received-tab">
               <div className="rbt-dashboard-table table-responsive mobile-table-750">
-                <table className="rbt-table table table-borderless">
+                <table className="rbt-table table">
                   <thead>
                     <tr>
                       <th>Date</th>
                       <th>Message</th>
+                      <th>
+                        <button type="button" className="btn btn-dark">
+                          Unread <span className="badge bg-light text-dark ms-1">{notifications.filter(item => item.isRead === false).length}</span>
+                        </button>
+                      </th>
                     </tr>
                   </thead>
                   <tbody>
-                    {notifications.length > 0 ? notifications.map(alert => ( 
-                      <tr key={alert.id} onClick={() => handleShowNotification(alert)}>
-                        <td>{alert.createdAt.split('T')[0]}</td>
-                        <td>
-                          <p className="b2">{alert.message}</p>
+                    {notifications.length > 0 ? (
+                      notifications.map(alert => (
+                        <tr key={alert.id} onClick={() => handleShowNotification(alert)} style={{cursor:'pointer'}}>
+                          <td>{alert.createdAt.split('T')[0]}</td>
+                          <td>
+                            <p className="b2" style={{fontWeight:`${alert.isRead ? '400':'700'}`}}>{alert.message}</p>
+                          </td>
+                          <td className="d-flex justify-content-center">
+                          {alert.isRead ? <i className="bi bi-eye-slash-fill" style={{fontSize:'1.2em'}}></i>:<i className="bi bi-eye-fill" style={{fontSize:'1.2em'}}></i>}
+                          </td>
+                        </tr>
+                      ))
+                    ) : (
+                      <tr>
+                        <td colSpan={2} className="text-center">
+                          <div className="m-4">Notifications will appear here</div>
                         </td>
                       </tr>
-                    )):<tr> <div className="m-4">notifications will appear here</div></tr>}
+                    )}
                   </tbody>
                 </table>
               </div>
             </div>
-           
+
           </div>
         </div>
       </div>
--- app/api/endpoints.ts
+++ app/api/endpoints.ts
@@ -2,4 +2,6 @@ export const deployedUrl = 'https://khumla-development-user-write.azurewebsites.
 
 export const readUserData = 'https://khumla-dev-user-read.azurewebsites.net';
 
-export const getnotificationUrl = 'https://khumla-dev-notification-read.azurewebsites.net'
+export const getnotificationUrl = 'https://khumla-dev-notification-read.azurewebsites.net';
+
+export const notificationWriteUrl = 'https://khumla-dev-notification-write.azurewebsites.net';
