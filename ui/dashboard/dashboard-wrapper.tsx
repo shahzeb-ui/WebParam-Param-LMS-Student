6ad0fd96960405bcde7276112ca5c4863d78450c
Bradley Matjie <77038935+bradleymatjie@users.noreply.github.com>
2024-07-24 10:24:20 +0200

    suggested

--- ui/dashboard/dashboard-wrapper.tsx
+++ ui/dashboard/dashboard-wrapper.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import thootoHeader from "./thoota.jpg"
 import "./userProfile.scss";
 
 const InstructorDashboardHeader = () => {
@@ -9,7 +10,7 @@ const InstructorDashboardHeader = () => {
       <div className="rbt-dashboard-content-wrapper">
         <div className="height-350 rbt-shadow-box" 
         style={{
-          backgroundImage:'url(https://htmlstream.com/preview/front-v4.3.1/assets/svg/illustrations/oc-relaxing.svg)',
+          backgroundImage: `url(${thootoHeader.src})`,
           backgroundRepeat:'no-repeat',
           backgroundPosition:'top',
           backgroundSize:'cover'
@@ -17,15 +18,15 @@ const InstructorDashboardHeader = () => {
         <div className="rbt-tutor-information">
           <div className="rbt-tutor-information-left">
             <div className="thumbnail rbt-avatars size-lg">
-              <Image
+              {/* <Image
                 width={300}
                 height={300}
                 src="https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg"
                 alt="Instructor"
-              />
+              /> */}
             </div>
             <div className="tutor-content">
-              <h5 className="title">John Due</h5>
+              {/* <h5 className="title">John Due</h5> */}
               {/* <div className="rbt-review">
                 <div className="rating">
                   <i className="fas fa-star" />
