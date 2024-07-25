7ad6d14847444432c2b59a141965b1a9007d289c
MatshepoFrak <matshepo.soto@fraktionaldev.com>
2024-07-25 14:15:54 +0200

    changes

--- app/(protected)/student/layout.tsx
+++ app/(protected)/student/layout.tsx
@@ -27,7 +27,7 @@ export default function StudentLayout({
   return (
     <>
       <div className="rbt-page-banner-wrapper">
-      {/* <div className="rbt-banner-image custom-banner" /> */}
+      
       </div>
       
       <div className="rbt-dashboard-area rbt-section-overlayping-top rbt-section-gapBottom">
