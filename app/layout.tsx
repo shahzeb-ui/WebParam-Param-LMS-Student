119200337d0dcf24d35c7c39eabe80482711210e
NjinuParam <njinu@webparam.org>
2024-08-14 21:32:13 +0200

    feat(fail)

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,7 @@ export const viewport: Viewport = {
 };
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
-  const pathname = usePathname();
+  const pathname = usePathna me();
   const [menuToggler, setMenuToggler] = useState(false);
   const [open, setOpen] = useState(false);
 
