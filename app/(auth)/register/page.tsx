f1586679038d47f3f0a8413fc47d18e8b526b3aa
Bradley Matjie <77038935+bradleymatjie@users.noreply.github.com>
2024-08-21 12:59:50 +0200

    register

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -19,7 +19,7 @@ export default function Register({searchParams}: {searchParams: {courseId: strin
     
     const cookies = new Cookies();
     const router = useRouter();
-    const hasConstantCourseId = process.env.NEXT_PUBLIC_COURSE_ID??"6669f0ff8759b480859c10a7";
+    const hasConstantCourseId = process.env.NEXT_PUBLIC_COURSE_ID;
 
 
     
@@ -27,7 +27,7 @@ export default function Register({searchParams}: {searchParams: {courseId: strin
         e.preventDefault();
         setIsSubmitted(true);
         const payload: registerType = {
-            courseId: hasConstantCourseId,
+            courseId: hasConstantCourseId??"6669f0ff8759b480859c10a7",
             email:email,
             phoneNumber:phone,
             username:username,
