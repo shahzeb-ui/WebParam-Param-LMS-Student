61c709067a11f8d4fac03d66c0e84d14ca2aba14
Bradley Matjie <77038935+bradleymatjie@users.noreply.github.com>
2024-08-22 11:39:19 +0200

    animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-"use client";
+'use client'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 import { Metadata } from "next";
 import styles from "./page.module.css";
@@ -10,6 +12,9 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(()=>{
+    AOS.init({
+      duration: 1200,
+    });
     router.push("/login")
   },[])
 
