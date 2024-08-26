0b53bc72e14f4907593675f751a77a97f5a9aa93
Bradley Matjie <77038935+bradleymatjie@users.noreply.github.com>
2024-08-26 10:05:36 +0200

    register and verify account pages

--- app/(protected)/student/student-profile/profile/page.tsx
+++ app/(protected)/student/student-profile/profile/page.tsx
@@ -226,7 +226,7 @@ export default function Profile({ student }: any) {
                     </div>
                 </div>
             </div>
-            <form onSubmit={handleSubmit} className="rbt-profile-row rbt-default-form row row--15">
+            <form onSubmit={handleSubmit} className="rbt-profile-row rbt-default-form row row--15" style={{minWidth:'100%'}}>
                 <div className="col-lg-6 col-md-6 col-sm-6 col-12">
                     <div className="rbt-form-group">
                         <label htmlFor="firstname">First Name</label>
