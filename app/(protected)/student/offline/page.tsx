2684d7fae10cc83b12d938e56b0dfdfd7cd25d87
MatshepoFrak <matshepo.soto@fraktionaldev.com>
2024-09-04 11:26:00 +0200

    Offline page

--- app/(protected)/student/offline/page.tsx
+++ app/(protected)/student/offline/page.tsx
@@ -1,25 +1,20 @@
-import type { Metadata } from "next";
-import "./offline.scss"; 
-
-export const metadata: Metadata = {
-  title: "Offline",
-};
+import Image from 'next/image';
+import "./offline.scss";
 
 export default function Page() {
   return (
     <div className="offline-container">
       <div className="offline-content">
-        <div className="icons">
-          <img src="/images/nowifi.gif" alt="No WiFi" className="offline-image" />
-        </div>
-        <h1>Oops! The internet took a coffee break. </h1>
-        <h2>Please connect to the internet and try again.</h2>
-        <button
-          className="rbt-btn btn-gradient hover-icon-reverse w-50"
-          style={{ background: 'transparent', border: '2px solid black' }}
-        >
-         <span className="btn-text">Retry</span>
-        </button>
+        <Image
+          src="/images/offline/nowifi.gif"
+          alt="No WiFi"
+          width={200}
+          height={200}
+          className="nowifi-gif"
+        />
+        <h1>Oops! The internet took a coffee break.</h1>
+        <p>Please connect to the internet and try again.</p>
+        <button className="retry-button">Retry</button>
       </div>
     </div>
   );
