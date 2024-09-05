edc7170a147c673cd24c21c56be8a2c3fe79a038
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-09-05 21:24:07 +0200

    fix: offline mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,22 @@
 import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
+  dest: "public", // Destination for the service worker
+  register: true,
+  skipWaiting: true,
+  disable: false,
   fallbacks: {
-    //image: "/static/images/fallback.png",
     document: "/offline", // if you want to fallback to a custom page rather than /_offline
-    // font: '/static/font/fallback.woff2',
-    // audio: ...,
-    // video: ...,
-  },
-  workboxOptions: {
-    disableDevLogs: true,
+    image: "/images/offline/nowifi.gif",
   },
 });
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+  output: "export",
   images: {
-      unoptimized: true,
+    unoptimized: true,
   },
 };
 
