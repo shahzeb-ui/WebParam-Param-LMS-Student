c5c3e1ab5cc3a6f0e888abe4c05786c14be5fb53
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-09-12 13:58:49 +0200

    fix: flagsmith integration

--- app/FlagSmithProvider.tsx
+++ app/FlagSmithProvider.tsx
@@ -1,25 +1,27 @@
-// "use client";
-// // export default function FlagSmithProvider({ }) { third }
-// import flagsmith from "flagsmith/isomorphic";
-// import { FlagsmithProvider } from "flagsmith/react";
+"use client";
+// export default function FlagSmithProvider({ }) { third }
+import flagsmith from "flagsmith/isomorphic";
+import { FlagsmithProvider } from "flagsmith/react";
 
-// export default async function FlagSmithProvider({
-//   children,
-// }: Readonly<{ children: React.ReactNode }>) {
-//   await flagsmith.init({
-//     // fetches flags on the server and passes them to the App
-//     environmentID: "TmQCzVJqriWe9USBFUFLyf",
-//   });
+export default async function FlagSmithProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  await flagsmith.init({
+    // fetches flags on the server and passes them to the App
+    environmentID: "TmQCzVJqriWe9USBFUFLyf",
+  });
 
-//   return (
-//     <FlagsmithProvider
-//       serverState={flagsmith.getState()}
-//       options={{
-//         environmentID: "TmQCzVJqriWe9USBFUFLyf",
-//       }}
-//       flagsmith={flagsmith}
-//     >
-//       <>{children}</>
-//     </FlagsmithProvider>
-//   );
-// }
+  return (
+    <FlagsmithProvider
+      serverState={flagsmith.getState()}
+      options={{
+        environmentID: "TmQCzVJqriWe9USBFUFLyf",
+      }}
+      flagsmith={flagsmith}
+    >
+      <>{children}</>
+    </FlagsmithProvider>
+  );
+}
