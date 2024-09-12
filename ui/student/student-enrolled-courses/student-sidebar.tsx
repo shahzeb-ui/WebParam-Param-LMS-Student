e50c2b96b61c061f4dbbc397ffd5e494f1ab2eb7
NjinuParam <njinu@webparam.org>
2024-09-12 22:38:26 +0200

    feat(up)

--- ui/student/student-enrolled-courses/student-sidebar.tsx
+++ ui/student/student-enrolled-courses/student-sidebar.tsx
@@ -10,8 +10,8 @@ const StudentDashboardSidebar = () => {
   const user = cookies.get("loggedInUser");
   const path = usePathname();
   const isFreemium =
-    process.env.NEXT_PUBLIC_IS_FREEMIUM &&
-    process.env.NEXT_PUBLIC_IS_FREEMIUM == "true"
+    process.env.NEXT_PUBLIC_FREEMIUM &&
+    process.env.NEXT_PUBLIC_FREEMIUM == "true"
       ? true
       : false;
 
@@ -49,10 +49,10 @@ const StudentDashboardSidebar = () => {
               <nav className="mainmenu-nav">
                 <ul className="dashboard-mainmenu rbt-default-sidebar-list">
                   {SidebarData &&
-                    SidebarData?.slice(0, 7).map((data: any, index: any) => {
+                    SidebarData.slice(0, isFreemium?3:8).map((data: any, index: any) => {
                       
                       console.log(path === '/student/projects?tab=enrolled');
-                      if (process.env.NEXT_PUBLIC_IS_FREEMIUM && data.link == "/student/enrolled-courses") {
+                      if (process.env.NEXT_PUBLIC_FREEMIUM && data.link == "/student/enrolled-courses") {
                         return  (
                         <li className="nav-item" key={index} role="presentation">
                         <a
@@ -80,7 +80,7 @@ const StudentDashboardSidebar = () => {
                 </ul>
               </nav>
 
-              {SidebarData?.length > 7 &&
+              {SidebarData?.length > (isFreemium?3:7) &&
                 <div className="section-title mt--40 mb--20">
                   <h6 className="rbt-title-style-2">User</h6>
                 </div>
@@ -88,7 +88,7 @@ const StudentDashboardSidebar = () => {
               <nav className="mainmenu-nav">
                 <ul className="dashboard-mainmenu rbt-default-sidebar-list">
                   {SidebarData &&
-                    SidebarData?.slice(7).map((data: any, index: any) => (
+                     SidebarData.slice(isFreemium?3:8, isFreemium?5:10).map((data: any, index: any) => (
                       <li key={index}>
                         <a
                           href={data.link}
