b7db50d08c8182657da6c58aa896a1e65ff0f654
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-09-12 16:55:56 +0200

    fix: flagsmith integration

--- app/FlagSmithProvider.tsx
+++ app/FlagSmithProvider.tsx
@@ -11,7 +11,6 @@ export default async function FlagSmithProvider({
 
   return (
     <FlagsmithProvider
-      serverState={flagsmith.getState()}
       options={{
         environmentID: "TmQCzVJqriWe9USBFUFLyf",
       }}
