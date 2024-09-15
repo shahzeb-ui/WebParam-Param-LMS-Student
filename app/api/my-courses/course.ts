2f14837ba2e23412db9ca335eb5f372ee7930a14
NjinuParam <njinu@webparam.org>
2024-09-15 22:21:03 +0200

    feat(up)

--- app/api/my-courses/course.ts
+++ app/api/my-courses/course.ts
@@ -1,10 +1,11 @@
+
 import { GET } from "@/app/lib/api-client";
 import { rCourseUrl } from "@/app/lib/endpoints";
 import axios from "axios";
 
 export async function getCourseId(userId: string) {
   // const courseId = axios.get(`${rCourseUrl}/Enrollments/GetUserEnrolledCourse/${userId}`);
-  
+  debugger;
   const courseId = GET(`${rCourseUrl}/Enrollments/GetUserEnrolledCourse/${userId}`);
 
   if (!courseId) {
@@ -16,7 +17,7 @@ export async function getCourseId(userId: string) {
 
 export async function getEnrolledCourse(getCourseId: any) {
   // const knowledgeModule = axios.get(`${rCourseUrl}/api/v1/KnowledgeModules/GetKnowledgeModules/${getCourseId}`);
-  
+  debugger;
   const knowledgeModule = GET(`${rCourseUrl}/api/v1/KnowledgeModules/GetKnowledgeModules/${getCourseId}`);
 
   if (!knowledgeModule) {
--- public/sw.js
+++ public/sw.js
@@ -79,10 +79,7 @@ define(['./workbox-1e54d6fe'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "/images/offline/nowifi.gif",
-    "revision": "development"
-  }, {
-    "url": "/offline",
+    "url": "/~offline",
     "revision": "development"
   }], {
     "ignoreURLParametersMatching": [/^utm_/, /^fbclid$/, /ts/]
