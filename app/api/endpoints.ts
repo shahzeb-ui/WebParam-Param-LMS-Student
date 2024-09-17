e52532a4a696657949e0999273a12e3d5f1927cc
KwaneleNdaba <113377563+KwaneleNdaba@users.noreply.github.com>
2024-09-17 17:49:36 +0200

    changed endpoints

--- app/api/endpoints.ts
+++ app/api/endpoints.ts
@@ -1,6 +1,6 @@
-export const writeUserData = 'https://khumla-dev-user-write.azurewebsites.net';
+export const writeUserData = 'https://khumla-production-user-write.azurewebsites.net';
 
-export const readUserData = 'https://khumla-dev-user-read.azurewebsites.net';
+export const readUserData = 'https://khumla-production-user-read.azurewebsites.net';
 
 export const getnotificationUrl = 'https://khumla-dev-notification-read.azurewebsites.net';
 
