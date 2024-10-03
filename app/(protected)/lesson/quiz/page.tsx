13f5b01a9c67fc60c4bcd4420d6f06c45b9916ab
NjinuParam <99141352+NjinuParam@users.noreply.github.com>
2024-10-03 08:43:27 +0200

    feat(up)

--- app/(protected)/lesson/quiz/page.tsx
+++ app/(protected)/lesson/quiz/page.tsx
@@ -6,8 +6,11 @@ import { accountingQuiz } from "@/data/quiz/accounting";
 import styles from "@/styles/quiz/quiz.module.css";
 import { useRouter } from "next/navigation";
 import './quiz.scss'
+import { POST } from "@/app/lib/api-client";
+import Loader from "@/ui/loader/loader";
+import Countdown from 'react-countdown';
 
-type QuizQuestion = {
+interface IQuizQuestion  {
   question: string;
   options: string[];
   answer: string;
@@ -17,6 +20,8 @@ const LessonQuiz = ({setVideoEnded, handleNext}:any) => {
   const [next, setNext] = useState<number>(0);
   const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
   const [score, setScore] = useState<number>(0);
+  const [error, setError] = useState<string>("");
+  const [loading, setLoader] = useState<boolean>(false);
   const [timeRemaining, setTimeRemaining] = useState<number>(600/60); 
   const [answeredQuestions, setAnsweredQuestions] = useState<boolean[]>(
     Array(quizData.length).fill(false)
@@ -24,7 +29,7 @@ const LessonQuiz = ({setVideoEnded, handleNext}:any) => {
   const [selectedAnswers, setSelectedAnswers] = useState<string[]>(
     Array(quizData.length).fill(null)
   );
-  const [currentQuiz, setCurrentQuiz] = useState<QuizQuestion[]>([]);
+  const [currentQuiz, setCurrentQuiz] = useState<IQuizQuestion[]>([]);
   const router = useRouter(); 
 
   useEffect(() => {
@@ -51,7 +56,25 @@ const LessonQuiz = ({setVideoEnded, handleNext}:any) => {
     };
   }, [timeRemaining, selectedAnswers]);
 
-  const initializeQuiz = () => {
+  // Random component
+const Completionist = () => <span>You are good to go!</span>;
+
+// Renderer callback with condition
+const renderer = ({ hours, minutes, seconds, completed }:any) => {
+  if (completed) {
+    // Render a completed state
+    return <Completionist />;
+  } else {
+    // Render a countdown
+    return <span>{hours}:{minutes}:{seconds}</span>;
+  }
+};
+
+
+  const initializeQuiz = async () => {
+    setLoader(true);
+   const questions = await getQuizQuestions();
+   debugger;
     const shuffledQuestions = accountingQuiz.sort(() => 0.5 - Math.random());
     const selectedQuestions = shuffledQuestions.slice(0, 10);
     setCurrentQuiz(selectedQuestions);
@@ -94,6 +117,24 @@ const LessonQuiz = ({setVideoEnded, handleNext}:any) => {
     }
   };
 
+  const getQuizQuestions=async ()=>{
+    const previousVideoId ="668ba8100c7b916b68693684";
+    const payload = {
+      videoId:previousVideoId
+    }
+    const res = await POST(payload,"https://thooto-qa-be-document-parser.azurewebsites.net/api/v1/topicQuiz/generate");
+    debugger;
+   
+    if(res == null){
+
+      setError("Error generating quiz, please try again.")
+    }
+    setLoader(false);
+    const result =  res?.data as IQuizQuestion[];
+    return result;
+    
+  }
+
   const handleRetake = () => {
     initializeQuiz();
   };
@@ -105,6 +146,44 @@ const LessonQuiz = ({setVideoEnded, handleNext}:any) => {
       <div className="inner">
         <div className="content">
             <div className="quiz-form-wrapper">
+              {loading && <>Generating quiz...<br/><br/><Loader/></>}
+              {error!==""&&   <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#ffffff",
+      }}
+    >
+      <div style={{ textAlign: "center", padding: "20px", maxWidth: "400px" }}>
+        {/* <img
+          src="/images/offline/nowifi.gif"
+          alt="No WiFi"
+          width={200}
+          height={200}
+          style={{ marginBottom: "20px" }}
+        /> */}
+        <h1>Oops! {error}.</h1>
+        <p>Please try again.</p>
+        <button
+        onClick={()=>{initializeQuiz()}}
+          style={{
+            backgroundColor: "#ffffff",
+            border: "1px solid #333",
+            color: "#333",
+            padding: "10px 20px",
+            fontSize: "16px",
+            borderRadius: "5px",
+            cursor: "pointer",
+            transition: "background-color 0.3s, color 0.3s",
+          }}
+          disabled={loading}
+        >
+          Retry
+        </button>
+      </div>
+    </div>}
                 {currentQuiz.map((item, index) => (
                     <div
                         key={index}
@@ -128,6 +207,10 @@ const LessonQuiz = ({setVideoEnded, handleNext}:any) => {
                                 <span>
                                 <i style={{color:"orange"}} className="feather-clock" />
                                 <small><b>Time remaining: </b>{timeRemaining.toFixed(2)} minutes</small>
+                                <Countdown
+    date={Date.now() + 5000}
+    renderer={renderer}
+  />,
                                 </span>
                             </div>
                         </div>
