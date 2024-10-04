2015fdcad50a836d378ea1bf9b39bccdd1557c1c
MatshepoFrak <matshepo.soto@fraktionaldev.com>
2024-10-04 07:54:41 +0200

    ...

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -6,37 +6,44 @@ const LoginPage = () => {
     <div className={styles.loginPage}>
       <LoginNav />
       <div className={styles.loginContainer}>
-      <div className={styles.loginFormWrapper}>
-          <h1 className={styles.title}>Hello <span className={styles.titleUnderline}>Again</span></h1>
-          <p className={styles.subtitle}>Welcome back! Please fill in your details.</p>
-          <form>
-            <div className={styles.formGroup}>
-              <input
-                type="email"
-                placeholder="Email Address"
-                className={`${styles.input} ${styles.requiredField}`}
-              />
-            </div>
-            <div className={styles.formGroup}>
-              <input
-                type="password"
-                placeholder="Password"
-                className={`${styles.input} ${styles.requiredField}`}
-              />
-            </div>
-            {/* Add error message if needed */}
-            <div className={styles.errorMessage}>
-              *Incorrect email or password. Try again
-            </div>
-            <div className={styles.buttonGroup}>
-              <button type="submit" className={styles.signUpButton}>
-                Sign Up
-              </button>
-              <div className={styles.recoverPassword}>
-                <a href="/recover-password">Recover Password</a>
+        <div className={styles.loginFormWrapper}>
+          <div>
+            <h1 className={styles.title}>Hello <span className={styles.titleUnderline}>Again</span></h1>
+            <p className={styles.subtitle}>Welcome back! Please fill in your details.</p>
+            <form>
+              <div className={styles.formGroup}>
+                <input
+                  type="email"
+                  placeholder="Email Address"
+                  className={`${styles.input} ${styles.requiredField}`}
+                />
+                <span className={styles.asterisk}>*</span>
               </div>
-            </div>
-          </form>
+              <div className={styles.formGroup}>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className={`${styles.input} ${styles.requiredField}`}
+                />
+                <span className={styles.asterisk}>*</span>
+              </div>
+              {/* Add error message if needed */}
+              <div className={styles.errorMessage}>
+                *Incorrect email or password. Try again
+              </div>
+              <div className={styles.buttonGroup}>
+                <button type="submit" className={styles.signUpButton}>
+                  Sign Up
+                </button>
+                <div className={styles.recoverPassword}>
+                  <a href="/recover-password">Recover Password</a>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className={styles.signUpPrompt} style={{ position: 'absolute', bottom: '20px', left: '0', right: '0', fontWeight: 'bold', fontSize: '16px', padding: '15px', color:'white' }}>
+            Don't have an account yet? <a href="/register" style={{ color: '#FE457A', textDecoration: 'underline' }}>Sign Up</a>
+          </div>
         </div>
       </div>
     </div>
--- ui/login/login-nav.tsx
+++ ui/login/login-nav.tsx
@@ -6,21 +6,21 @@ const LoginNav = () => {
   return (
     <div className={styles.root}>
       <nav className={styles.navbar}>
-        <div className={styles.navbarContainer}>
-          <button className={styles.menuButton}>
+      <div className={styles.navbarContainer}>
+        <button className={styles.menuButton} style={{ float: 'left', marginRight: 'auto', position: 'absolute', left: '20px' }}>
             <i className="fas fa-bars"></i>
-          </button>
-          <ul className={styles.navLinks}>
-            <li><Link href="#platform">Platform</Link></li>
-            <li><Link href="#programmes">Programmes</Link></li>
-            <li>
-              <Link href="/">
-                <span className={`${styles.logo} ${styles.logoWrapper}`}>
+        </button>
+          <ul className={styles.navLinks} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', listStyleType: 'none', padding: 0, margin: '0 auto', position: 'absolute', left: '52%', transform: 'translateX(-50%)', zIndex: '1000 !important' }}>
+            <li style={{ margin: '0 15px' }}><Link href="#platform" style={{ textDecoration: 'none', color: '#333 !important' }}>Platform</Link></li>
+            <li style={{ margin: '0 15px' }}><Link href="#programmes" style={{ textDecoration: 'none', color: '#333 !important' }}>Programmes</Link></li>
+            <li style={{ margin: '0 15px' }}>
+              <Link href="/" style={{ textDecoration: 'none' }}>
+                <span className={`${styles.logo} ${styles.logoWrapper}`} style={{ display: 'inline-block', fontSize: '24px', color: '#333 !important' }}>
                   <span className={styles.logoText}>R</span>
                 </span>
               </Link>
             </li>
-            <li><Link href="#contact-us">Contact Us</Link></li>
+            <li style={{ margin: '0 15px' }}><Link href="#contact-us" style={{ textDecoration: 'none', color: '#333 !important' }}>Contact Us</Link></li>
           </ul>
           <div className={styles.authLinks}>
             <span className={styles.userIcon}>
